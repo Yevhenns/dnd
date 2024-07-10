dcab34967e86b29ff1efb67bbd195012cb6f19fb
Yevhenns <gifestum@gmail.com>
2024-07-10 12:47:58 +0300

    added sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,65 @@
-import css from "./App.module.css";
-import { useEffect, useState } from "react";
-import { data } from './data'
-import { DropResult } from "react-beautiful-dnd";
-import { Tab } from "./components/Tab/Tab";
+import css from './App.module.scss';
+import { useEffect, useState } from 'react';
+import { data } from './data';
+import { DropResult } from 'react-beautiful-dnd';
+import { Tab } from './components/Tab/Tab';
 
 export type DataItem = {
-    id: string;
-    title: string;
-    pinned: boolean;
-    route: string
+  id: string;
+  title: string;
+  pinned: boolean;
+  route: string;
 };
 
 function App() {
-    const [pinnedItems, setPinnedItems] = useState<DataItem[]>([]);
-    const [unpinnedItems, setUnpinnedItems] = useState<DataItem[]>([]);
+  const [pinnedItems, setPinnedItems] = useState<DataItem[]>([]);
+  const [unpinnedItems, setUnpinnedItems] = useState<DataItem[]>([]);
 
-    useEffect(() => {
-        const array = data.filter((item) => item.pinned === true);
-        setPinnedItems(array);
-    }, []);
+  useEffect(() => {
+    const array = data.filter(item => item.pinned === true);
+    setPinnedItems(array);
+  }, []);
 
-    useEffect(() => {
-        const array = data.filter((item) => item.pinned === false);
-        setUnpinnedItems(array);
-    }, []);
+  useEffect(() => {
+    const array = data.filter(item => item.pinned === false);
+    setUnpinnedItems(array);
+  }, []);
 
-    function handleOnDragEnd(result: DropResult) {
+  function handleOnDragEnd(result: DropResult) {
+    if (!result.destination) return;
 
-        if (!result.destination) return;
+    const items = Array.from(pinnedItems);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
 
-        const items = Array.from(pinnedItems);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
+    setPinnedItems(items);
+  }
 
-        setPinnedItems(items);
-    }
+  function handleOnDragEnd1(result: DropResult) {
+    if (!result.destination) return;
 
-    function handleOnDragEnd1(result: DropResult) {
+    const items = Array.from(unpinnedItems);
+    const [reorderedItem] = items.splice(result.source.index, 1);
+    items.splice(result.destination.index, 0, reorderedItem);
 
-        if (!result.destination) return;
+    setUnpinnedItems(items);
+  }
 
-        const items = Array.from(unpinnedItems);
-        const [reorderedItem] = items.splice(result.source.index, 1);
-        items.splice(result.destination.index, 0, reorderedItem);
-
-        setUnpinnedItems(items);
-    }
-
-    return (
-        <div className={css.layout} >
-            <Tab heading="pinned" droppableId="pinned" data={pinnedItems} handleOnDragEnd={handleOnDragEnd} />
-            <Tab heading="unpinned" droppableId="unpinned" data={unpinnedItems} handleOnDragEnd={handleOnDragEnd1} />
-
-        </div>
-    );
+  return (
+    <div className={css.layout}>
+      <Tab
+        droppableId="pinned"
+        data={pinnedItems}
+        handleOnDragEnd={handleOnDragEnd}
+        pinned
+      />
+      <Tab
+        droppableId="unpinned"
+        data={unpinnedItems}
+        handleOnDragEnd={handleOnDragEnd1}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Tab/Tab.tsx
+++ src/components/Tab/Tab.tsx
@@ -1,42 +1,59 @@
-import { Link } from "react-router-dom";
-import { DataItem } from "../../App";
-import css from './Tab.module.css'
-import { DragDropContext, Draggable, Droppable, DropResult } from "react-beautiful-dnd";
+import { Link } from 'react-router-dom';
+import { DataItem } from '../../App';
+import css from './Tab.module.scss';
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from 'react-beautiful-dnd';
 
 type TabProps = {
-    heading: string;
-    data: DataItem[];
-    handleOnDragEnd: (result: DropResult) => void;
-    droppableId: string
+  data: DataItem[];
+  handleOnDragEnd: (result: DropResult) => void;
+  droppableId: string;
+  pinned?: boolean;
 };
 
-export function Tab({ data, heading, handleOnDragEnd, droppableId }: TabProps) {
-    return (
-        <div className={css.layout}>
-            <span>{heading}</span>
-            <DragDropContext onDragEnd={handleOnDragEnd}>
-                <Droppable droppableId={droppableId} type='group' direction="horizontal">
-                    {(provided) => (
-                        <div className={css.wrapper} {...provided.droppableProps} ref={provided.innerRef}>
-                            {data.map((item, index) => {
-                                return (
-                                    <Draggable key={item.id} draggableId={item.id} index={index}>
-                                        {(provided) => (
-                                            <div className={css.item} {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}>
-                                                <Link to={item.route} key={item.id}>
-                                                    <span>{item.title}</span>
-                                                </Link></div>
-                                        )}
-
-                                    </Draggable>
-                                );
-                            })}
-                            {provided.placeholder}
-                        </div>
-                    )}
-
-                </Droppable>
-            </DragDropContext>
-        </div >
-    );
+export function Tab({
+  data,
+  handleOnDragEnd,
+  droppableId,
+  pinned = false,
+}: TabProps) {
+  return (
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <Droppable droppableId={droppableId} type="group" direction="horizontal">
+        {provided => (
+          <div
+            className={[css.wrapper, pinned ? css.pinned : css.unpinned].join(
+              ' '
+            )}
+            {...provided.droppableProps}
+            ref={provided.innerRef}
+          >
+            {data.map((item, index) => {
+              return (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {provided => (
+                    <div
+                      className={css.item}
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                    >
+                      <Link to={item.route} key={item.id}>
+                        <span>{item.title}</span>
+                      </Link>
+                    </div>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </div>
+        )}
+      </Droppable>
+    </DragDropContext>
+  );
 }
